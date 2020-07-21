cf43d699e8a99385dd99ca0c86ef34586aa180de
NataliaVolik <log.litana@gmail.com>
2020-07-21 13:41:32 +0600

    display list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interface/hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,16 +8,18 @@ import { Hero } from '../interface/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
+  heroes = HEROES;
+  selectedHero: Hero;
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
